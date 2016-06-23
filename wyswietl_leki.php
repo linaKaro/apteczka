62f6103c196d0849568b7dfc716a1d3173835129
annakica <anna.kica@gmail.com>
2016-06-23 21:36:42 +0200

    Update wyswietl_leki.php

--- wyswietl_leki.php
+++ wyswietl_leki.php
@@ -16,11 +16,94 @@
 	<p><?php echo $podtytul ?></p>
 </div>
 
+
+
+<div class="page-header">Wyszukaj wybrany lek w bazie</div>
+<form action="" method="post">
+    <div class="row">
+        <div class="col-md-4">
+            <label for="search_by">Wyszukaj po:</label>
+            <select class="form-control" id="search_by" name="metoda_szukania">
+                <option value="EAN">EAN</option>
+                <option value="nazwa_leku">Nazwa leku</option>
+            </select>
+        </div>
+        <div class="col-md-4">
+            <label for="enter_string">Szukaj leku:</label>
+            <input class="form-control" type="text" id="enter_string" name="wyrazenie">
+        </div>
+        <div class="col-md-4">
+            <br>
+            <button type="submit" class="btn btn-primary">Wyszukaj</button>
+        </div>
+    </div>
+    </br></br>
+</form>
+
 <?php 
 	if (!isset($_GET['wybrano'])) {
 		header("Location: myindex.php?wybrano=0&zaloguj_sie=1");
 	}
 	
+?>
+
+<?php
+
+	$metoda_szukania=$_POST['metoda_szukania'];
+	$wyrazenie = $_POST['wyrazenie']; 
+
+	
+	$zapytanie = "select * from leki where " .$metoda_szukania. " like'%".$wyrazenie."%' ";
+	$rezultat = $baza->query($zapytanie);
+	
+?>
+
+<div class="table-responsive">
+<table class="table" id="tabela1">
+	<thead>
+	<tr>
+		<td>
+		<?php echo "ID leku"; ?> </td>
+		<td id="tabela1">
+		<?php echo "Nazwa";?> </td>
+		<td id="tabela1">
+		<?php echo "EAN";?> </td>
+		<td id="tabela1">
+		<?php echo "Ilość w opakowaniu";?> </td>
+		<td id="tabela1">
+		<?php echo "Jednostka";?> </td>
+		<td id="tabela1">
+		<?php echo "Substancja czynna";?> </td>
+	</tr>
+	</thead>
+
+<?php
+	$nr_rows = $rezultat->num_rows;
+	for($i=0;$i<$nr_rows;$i++){
+	$row=$rezultat->fetch_assoc();	
+	
+?>
+	<tr id="tabela1">
+		<td id="tabela1">
+		<?php echo $row["id"] . "\t"; ?> </td>
+		<td id="tabela1">
+		<?php echo $row["nazwa_leku"] . "\t";?> </td>
+		<td id="tabela1">
+		<?php echo $row["EAN"] . "\t";?> </td>
+		<td id="tabela1">
+		<?php echo $row["ilosc"] . "\t";?> </td>
+		<td id="tabela1">
+		<?php echo $row["jednostka"] . "\t";?> </td>
+		<td id="tabela1">
+		<?php echo $row["substancja_czynna"]. "<br>";?> </td>
+	</tr>
+<?php
+}
+?>
+</table>
+</div>
+
+<?php
 	
 	$query = "select * from leki";
 	
@@ -28,21 +111,24 @@
 	
 	echo "<br><br>REKORDY W BAZIE<br><br>";
 ?>
-<table>
-<tr>
-<td>
-	<?php echo "ID leku"; ?> </td>
-	<td id="tabela">
-	<?php echo "Nazwa";?> </td>
-	<td id="tabela">
-	<?php echo "EAN";?> </td>
-	<td id="tabela">
-	<?php echo "Ilość w opakowaniu";?> </td>
-	<td id="tabela">
-	<?php echo "Jednostka";?> </td>
-	<td id="tabela">
-	<?php echo "Substancja czynna";?> </td>
+<div class="table-responsive">
+<table class="table">
+	<thead>
+	<tr>
+		<td>
+		<?php echo "ID leku"; ?> </td>
+		<td id="tabela">
+		<?php echo "Nazwa";?> </td>
+		<td id="tabela">
+		<?php echo "EAN";?> </td>
+		<td id="tabela">
+		<?php echo "Ilość w opakowaniu";?> </td>
+		<td id="tabela">
+		<?php echo "Jednostka";?> </td>
+		<td id="tabela">
+		<?php echo "Substancja czynna";?> </td>
 	</tr>
+	</thead>
 
 <?php
 	while ($row = $wynik->fetch_assoc()) {
@@ -65,6 +151,7 @@
 }
 ?>
 </table>
+</div>
 
 <?php
 require_once 'inc/stopka.php';
